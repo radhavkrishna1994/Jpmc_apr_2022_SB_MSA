d593f17e8654dd84ffa46559b6a98b9f4c22a769
radha <radhavkrishna1994@gmail.com>
2022-04-25 17:11:37 +0530

    updated

--- spring-boot-demo/src/main/java/com/training/services/BookService.java
+++ spring-boot-demo/src/main/java/com/training/services/BookService.java
@@ -1,6 +1,7 @@
 package com.training.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,25 +24,44 @@ public class BookService implements BookServiceI {
 
 	@Override
 	public Book saveBook(Book book) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return bookRepo.save(book);
 	}
 
 	@Override
 	public Book getBook(Long isbn) {
-		// TODO Auto-generated method stub
+		
+		Optional<Book> opBook = bookRepo.findById(isbn);
+		if(opBook.isPresent())
+			return opBook.get();
+		
 		return null;
 	}
 
 	@Override
 	public Book deleteBook(Long isbn) {
-		// TODO Auto-generated method stub
+		Optional<Book> opBook = bookRepo.findById(isbn);
+		Book bookFound = null;
+		if(opBook.isPresent())
+		{
+			bookFound=opBook.get();
+			bookRepo.delete(bookFound);
+			return bookFound;
+		}
 		return null;
 	}
 
 	@Override
 	public Book updateBookStock(Long isbn, Long newStock) {
-		// TODO Auto-generated method stub
+		Optional<Book> opBook = bookRepo.findById(isbn);
+		Book bookFound = null;
+		if(opBook.isPresent())
+		{
+			bookFound=opBook.get();
+			bookFound.setStock(bookFound.getStock()+newStock);
+			bookRepo.save(bookFound);
+			return bookFound;
+		}
 		return null;
 	}
 
