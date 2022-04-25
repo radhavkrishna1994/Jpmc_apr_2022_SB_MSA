fcf7231ce6bd05240f94a26db29d6bea07377ffd
radha <radhavkrishna1994@gmail.com>
2022-04-25 17:30:33 +0530

    updated

--- spring-boot-demo/src/main/java/com/training/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/training/SpringBootDemoApplication.java
@@ -3,6 +3,7 @@ package com.training;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,7 @@ import com.training.model.Book;
 import com.training.repo.BookRepo;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
@@ -25,6 +26,16 @@ public class SpringBootDemoApplication {
 		bookRepo.save(new Book(1234l, "Alchemist", 225.50, 100l));
 		bookRepo.save(new Book(4467l, "Think like a Monk", 325.50, 100l));
 		bookRepo.save(new Book(6789l, "One Minute Manager", 255.50, 100l));
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println(bookRepo.findByTitle("Alchemist"));
+		
+		System.out.println(bookRepo.findByTitleLike("%like%"));
+		System.out.println(bookRepo.findByStockLessThan(150l));
 		
 	}
 	
--- spring-boot-demo/src/main/java/com/training/repo/BookRepo.java
+++ spring-boot-demo/src/main/java/com/training/repo/BookRepo.java
@@ -1,5 +1,7 @@
 package com.training.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.training.model.Book;
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
 
+	public List<Book> findByTitle(String title);
+	public List<Book> findByTitleLike(String title);
+	public List<Book> findByStockLessThan(Long Stock);
+	
 }
