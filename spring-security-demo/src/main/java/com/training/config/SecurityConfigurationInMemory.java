88b368008196a1efd51c25c2d70a25f27dc68ca6
radha <radhavkrishna1994@gmail.com>
2022-04-26 14:11:51 +0530

    Update SecurityConfigurationInMemory.java

--- spring-security-demo/src/main/java/com/training/config/SecurityConfigurationInMemory.java
+++ spring-security-demo/src/main/java/com/training/config/SecurityConfigurationInMemory.java
@@ -2,6 +2,7 @@ package com.training.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -20,11 +21,22 @@ public class SecurityConfigurationInMemory extends WebSecurityConfigurerAdapter 
 		.withUser("admin")
 		.password("{noop}admin")
 		.roles("ADMIN");
-						
+				
 	}
-	
-	
+		
 	//authorization
+	public void configure(HttpSecurity http) throws Exception
+	{
+		http
+		.authorizeRequests()
+		.antMatchers("/user/**")
+		.hasRole("USER")
+		.antMatchers("/admin/**")
+		.hasRole("ADMIN")
+		.and()
+		.formLogin();
+		
+	}
 	
 	
 
