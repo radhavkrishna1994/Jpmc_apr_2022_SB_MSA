4e4d26f5005917afb490c2c1644ad0895c6d1580
radha <radhavkrishna1994@gmail.com>
2022-04-27 17:15:44 +0530

    updated

---
+++ spring-boot-producer/src/main/java/com/training/model/Book.java
@@ -0,0 +1,28 @@
+package com.training.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="books")
+public class Book {
+
+	@Id
+	private Long isbn;
+	
+	@Column(name="title",length = 100,nullable = false)
+	private String title;
+	private Double price;
+	private Long stock;
+	
+	
+}
