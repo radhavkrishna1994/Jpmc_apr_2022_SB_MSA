9a8de7145c227ab9e896818bdfff6dea1dbc2c25
radha <radhavkrishna1994@gmail.com>
2022-04-27 10:31:16 +0530

    Jms Project added

---
+++ spring-jms-demo/src/main/java/com/training/config/JmsConfiguration.java
@@ -0,0 +1,21 @@
+package com.training.config;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JmsConfiguration {
+	
+	@Value("${test-queue}")
+	private String testQueue;
+	
+	@Bean
+	public ActiveMQQueue getActiveMQ()
+	{
+		return new ActiveMQQueue(testQueue);
+	}
+
+}
